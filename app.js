9575bab109174cdd7ac80c47e6b3d79bcd438b14
Kim T <kim200@Kims-MacBook-Pro.local>
2019-04-22 14:41:15 -0700

    Refactor GroceryList to render an array of groceryItem

--- app.js
+++ app.js
@@ -10,12 +10,36 @@ clearInterval// TODO
 //Create simple GroceryList components
 //that can take arguments by it's instance
 
-var GroceryList = () => (
-    <ul>
-        <li>Strawberry</li>
-        <li>Lemonade</li>
-    </ul>
-)
+// var GroceryList = () => (
+//     <ul>
+//         <li>Strawberry</li>
+//         <li>Lemonade</li>
+//     </ul>
+// )
 //Render simple GroceryList components
 // Template--: ReactDOM.render(componentInstance, DOMElement)
+
+// ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+
+//-------------
+// Create a reusable GroceryListItem component
+var GroceryListItem = (props) => (
+    <ul>
+        <li>{props.lists[0]}</li>
+        <li>{props.lists[1]}</li>
+    </ul>
+);
+
+var GroceryList = () => (
+    <div>
+        <h3>Reusable React components</h3>
+        <GroceryListItem lists={['Kale', 'Romaine']}/>
+    </div>
+);
+
 ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+
+// Refactor GroceryList to dynamically render an array of groceryItems,
+// utilizing your new GroceryListItem component
+
+
