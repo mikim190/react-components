25500903823535d251e222d2e532480770d5d4d0
Kim T <kim200@Kims-MacBook-Pro.local>
2019-04-22 15:51:05 -0700

    Handling User Events

--- app.js
+++ app.js
@@ -23,30 +23,30 @@ clearInterval// TODO
 
 //-------------
 // Create a reusable GroceryListItem component
-var GroceryListItem = (props) => {
+// var GroceryListItem = (props) => {
 
-    var onListItemClick = (event) => {
-        alert('Item got clicked!'); //click on first <li> will alert this message
-    };
+//     var onListItemClick = (event) => {
+//         alert('Item got clicked!'); //click on first <li> will alert this message
+//     };
 
-    return (
-    <ul>
-        <li onClick={onListItemClick}>{props.lists[0]}</li>
-        <li>{props.lists[1]}</li>
-        <li>{props.lists[2]}</li>
-    </ul>
-    );
-}
+//     return (
+//     <ul>
+//         <li onClick={onListItemClick}>{props.lists[0]}</li>
+//         <li>{props.lists[1]}</li>
+//         <li>{props.lists[2]}</li>
+//     </ul>
+//     );
+// }
 
-var GroceryList = () => (
-    <div>
-        <h3>Reusable React components</h3>
-        <GroceryListItem lists={['Kale', 'Romaine','Carrot']}/> 
+// var GroceryList = () => (
+//     <div>
+//         <h3>Reusable React components</h3>
+//         <GroceryListItem lists={['Kale', 'Romaine','Carrot']}/> 
         
-    </div>
-);
+//     </div>
+// );
 
-ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+// ReactDOM.render(<GroceryList/>, document.getElementById('app'));
 
 
 //JSX version for React
@@ -54,4 +54,32 @@ ReactDOM.render(<GroceryList/>, document.getElementById('app'));
 // Refactor GroceryList to dynamically render an array of groceryItems,
 // utilizing your new GroceryListItem component
 
+//-------ES6---------
+// var listOfItems = ['Kale','Romaine'];
+class GroceryListItem extends React.Component {
+
+    constructor(props) {
+      // Equivalent to ES5's React.Component.call(this, props)
+      super(props);
+      
+      
+    }
+
+    render() {
+      return (
+        <li>{this.props.list}</li>
+      );
+    } 
+  }
+  
+  var GroceryList = (props) => (
+      <ul>
+          {props.lists.map(list => 
+          <GroceryListItem list={list}/>
+        )}
+      </ul>
+    );
+
+  
+  ReactDOM.render(<GroceryList lists={['Milk','Berry','Apple']}/>, document.getElementById("app"));
 
