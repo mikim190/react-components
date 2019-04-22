ba6fadd20bf310f4ab8c7e705986b9b425d5244d
Kim T <kim200@Kims-MacBook-Pro.local>
2019-04-22 12:33:58 -0700

    Create and Render simple GroceryList components

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+/*
+create a GroceryList component that:
+- contains an unordered list of 2 grocery items. 
+- Render this component to the div tag in index.html with an id of app
+*/
+
+
+clearInterval// TODO
+
+//Create simple GroceryList components
+//that can take arguments by it's instance
+
+var GroceryList = () => (
+    <ul>
+        <li>Strawberry</li>
+        <li>Lemonade</li>
+    </ul>
+)
+//Render simple GroceryList components
+// Template--: ReactDOM.render(componentInstance, DOMElement)
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
